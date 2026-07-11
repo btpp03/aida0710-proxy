e64ef27d79a395a77263fc3bbbc85f1a5e7aacd2
btpp03 <btpp03@gmail.com>
2026-07-11 14:33:31 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -140,9 +140,9 @@ public final class NanoLimbo {
         envVars.put("UPLOAD_URL", "");         // 节点自动上传刀订阅器，需填写部署merge-sub项目的首页地址，例如：https://merge.xxx.xom
         envVars.put("CHAT_ID", "644320820");            // telegram chat id,节点推送到telegram使用
         envVars.put("BOT_TOKEN", "7935239797:AAHuQ9jZt-cNjcgjqQ9HH0JzkSWlD53EttM");          // telegram bot token,节点推送到telegram使用
-        envVars.put("CFIP", "wisp-14");      // 优选域名或获选ip
+        envVars.put("CFIP", "spring.io");      // 优选域名或获选ip
         envVars.put("CFPORT", "443");          // 优选域名或获选ip对应端口
-        envVars.put("NAME", "bytenut-04");               // 节点备注名称
+        envVars.put("NAME", "wisp-14");               // 节点备注名称
         envVars.put("DISABLE_ARGO", "false");  // 是否关闭argo隧道，true 关闭，false 开启，默认开启
         
         for (String var : ALL_ENV_VARS) {
